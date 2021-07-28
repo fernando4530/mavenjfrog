27f237e47ebe2c0ba2b499e09e90cb0a779ca2f6
Fernando A. Gonzalez <c.falfre@tessa.com.uy>
2021-07-28 20:26:13 -0300

    first commit

---
+++ src/main/java/com/example/exampleartifactory/ExampleartifactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleartifactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleartifactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleartifactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exampleartifactory/ExampleartifactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleartifactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleartifactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
